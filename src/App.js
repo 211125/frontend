9758c32d6c5df1971c9c5a0e89ec1e2647eca3b5
Frontend-CS <211125@ids.upchiapas.edu.mx>
2022-12-01 04:44:54 -0600

    hecho

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HomeUser from './controller/HomeUser';
+import Coperativa from './controller/Coperativa';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+   
        <HomeUser/>
       </header>
     </div>
